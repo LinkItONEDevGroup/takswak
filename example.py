eb5c04a2209f8a2f7ed95a5f71f41f9995ce1a7d
哈爸 <wuulong@gmail.com>
2022-10-17 09:44:16 +0800

    new: version V0.0.2
    enable RX
    enable user_reg

--- example.py
+++ example.py
@@ -8,8 +8,9 @@ from configparser import ConfigParser
 import pytak
 from os.path import exists
 
+VERSION="0.0.2"
 
-class MySerializer(pytak.QueueWorker):
+class MyTx(pytak.QueueWorker):
     """
     Defines how you process or generate your Cursor-On-Target Events.
     From there it adds the COT Events to a queue for TX to a COT_URL.
@@ -29,28 +30,37 @@ class MySerializer(pytak.QueueWorker):
         """
         sent_cnt=0
         while 1:
-            data = tak_pong3()
+            data = cot_from_file()
             if data:
                 print("sent_cnt=%i" %(sent_cnt))
+                sent_cnt+=1
                 await self.handle_data(data)
             await asyncio.sleep(20)
 
-
-def tak_pong():
+class MyRx(pytak.QueueWorker):
     """
-    Generates a simple takPong COT Event.
+    Defines how you process or generate your Cursor-On-Target Events.
+    From there it adds the COT Events to a queue for TX to a COT_URL.
     """
-    root = ET.Element("event")
-    root.set("version", "2.0")
-    root.set("type", "t-x-d-d")
-    root.set("uid", "takPong")
-    root.set("how", "m-g")
-    root.set("time", pytak.cot_time())
-    root.set("start", pytak.cot_time())
-    root.set("stale", pytak.cot_time(3600))
-    return ET.tostring(root)
 
-def tak_pong2():
+
+    async def run(self, number_of_iterations=-1):
+        """
+        Runs the loop for processing or generating pre-COT data.
+        """
+        rec_cnt=0
+        while 1:
+            if not self.queue.empty():
+                data = self.queue.get_nowait()
+                if data:
+                    print("rec_cnt=%i" %(rec_cnt))
+                    print(data)
+                    rec_cnt+=1
+            await asyncio.sleep(1)
+
+
+
+def cot_example():
     """
     Generates a simple takPong COT Event.
     """
@@ -70,7 +80,7 @@ def tak_pong2():
     point.set("le","9999999.0")
     return ET.tostring(root)
 
-def tak_pong3():
+def cot_from_file():
     #msg="""<event version='2.0' uid='a9e3bb10-17f8-4715-bc50-323063e587fa' type='a-u-A' time='2022-10-12T15:08:22.209Z' start='2022-10-12T15:08:22.209Z' stale='2023-10-12T15:08:22.209Z' how='h-g-i-g-o'><point lat='24.7946371' lon='121.0261352' hae='70.542' ce='9999999.0' le='9999999.0' /><detail><status readiness='true'/><archive/><archive/><contact callsign='airman 1'/><precisionlocation altsrc='DTED0'/><remarks></remarks><usericon iconsetpath='34ae1613-9645-4222-a9d2-e5f243dea2865/Military/airman.png'/><link uid='ANDROID-0d335e9f05caaf2a' production_time='2022-10-12T15:03:49.328Z' type='a-f-G-U-C' parent_callsign='wuulong' relation='p-p'/><color argb='-1'/></detail></event>"""
     if exists("msg.cot"):
         with open("msg.cot") as f:
@@ -78,13 +88,15 @@ def tak_pong3():
         msg="".join(lines).strip()
         return bytes(msg,'utf-8')
     return None
+
 async def main():
     """
     The main definition of your program, sets config params and
     adds your serializer to the asyncio task list.
     """
     config = ConfigParser()
-    config["mycottool"] = {"COT_URL": "tcp://34.80.169.66:8087"}
+
+    config["mycottool"] = {"COT_URL": "tcp://127.0.0.1:8087"}
     config = config["mycottool"]
 
     # Initializes worker queues and tasks.
@@ -92,7 +104,7 @@ async def main():
     await clitool.setup()
 
     # Add your serializer to the asyncio task list.
-    clitool.add_tasks(set([MySerializer(clitool.tx_queue, config)]))
+    clitool.add_tasks(set([MyTx(clitool.tx_queue, config),MyRx(clitool.rx_queue, config)]))
 
     # Start all tasks.
     await clitool.run()
